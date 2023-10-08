05c2ddcaef04cfd5847721be1b94ab3822299577
Filip Lazarevic <filipca@gmail.com>
2023-10-08 22:46:28 +0200

    Update 2-blinker-on-button-press.py

--- 2-blinker-on-button-press.py
+++ 2-blinker-on-button-press.py
@@ -1,11 +1,11 @@
 import time
 from machine import Pin
 
-state = Pin(20, Pin.IN)
+button = Pin(20, Pin.IN)
 led = Pin(25, Pin.OUT)
 
 while True:
-    if state.value():
+    if button.value():
         led.high()
     else:
         led.low()
