ece3583c46329ce85cf10081a9514bbdad3b8796
Filip Lazarevic <filipca@gmail.com>
2023-10-11 10:59:47 +0200

    Update 3-distance.py

--- 3-distance.py
+++ 3-distance.py
@@ -16,7 +16,7 @@ while True:
         pulse_end = time.ticks_us()
     pulse_duration = pulse_end - pulse_start
     # sound travels 0.343 meters per millisecond or 0.0343cm per milisecond
-    distance = (timepassed * 0.0343) / 2
+    distance = (pulse_duration * 0.0343) / 2
     distance = round(distance, 0)
     print ('Distance:',"{:.0f}".format(distance),'cm')
     time.sleep(1)
